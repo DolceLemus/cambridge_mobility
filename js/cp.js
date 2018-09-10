db2e283b18dff3f488502d8e17048bbc3b4a2e0a
Lizbethperez <lizbeth_perez_v@hotmail.com>
2018-09-10 13:33:48 -0500

    Se sube avances en el punto de localizacion del codigo postal

--- js/cp.js
+++ js/cp.js
@@ -1,5 +1,6 @@
 
-let cp="CB10AB"
+
+let cp="CB11AB";
     const regex=/^([Cc][Bb]1[0-1][A-Za-z]{2})$/;
         console.log(regex);
     if (regex.exec(cp) != null) {
@@ -8,5 +9,16 @@ let cp="CB10AB"
     }else{
         console.log("false");
     }
+
+
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(userUbication=(cp)=>{
+            let ubication={
+                lat:cp.coords.latitude,
+                lng:cp.coords.longitude
+            }
+            console.log(ubication);
+        })
+}
 
   
