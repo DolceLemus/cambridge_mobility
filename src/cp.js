a743e0913b86d001ec6f246e0f39f0e3c5d59f54
DolceLemus <dulce.lemus.lara@gmail.com>
2018-09-11 09:48:42 -0500

    version que funciona correctamente

--- src/cp.js
+++ src/cp.js
@@ -11,56 +11,56 @@ $("#search").click(function (event) {
 })
 
 
-const validateCp=(cp)=>{
-    const regex=/^([A-Za-z]{2}[0-9]{1,2}\s[0-9][A-Za-z]{2})$/;
-        console.log(regex);
-    if (regex.exec(cp) != null){
-        let cpWithSpace=cp.replace(" ","%");
+const validateCp = (cp) => {
+    const regex = /^([A-Za-z]{2}[0-9]{1,2}\s[0-9][A-Za-z]{2})$/;
+    console.log(regex);
+    if (regex.exec(cp) != null) {
+        let cpWithSpace = cp.replace(" ", "%");
         console.log(cpWithSpace);
         getLocationCp(cp);
         return true;
     }
-    else{
-        alert("zip code invalidate");
+    else {
+        // alert("zip code invalidate");
         return false;
-    }      
+    }
 }
-const getLocationCp=(dataCp)=>{
+const getLocationCp = (dataCp) => {
     const getCpCoords = new XMLHttpRequest();
     getCpCoords.open('GET', `https://maps.googleapis.com/maps/api/geocode/json?address=${dataCp}`);
     getCpCoords.onload = getGeolocation;
     getCpCoords.onerror = handleError;
     getCpCoords.send();
-  }
+}
 // Geolocation Function
-function getGeolocation(){
+function getGeolocation() {
     console.log(this.responseText);
     const data = JSON.parse(this.responseText);
-    let results=data.results;
+    let results = data.results;
     results.forEach(element => {
-       console.log(element.geometry.location);
-       let ubication={
-        lat:element.geometry.location.lat,
-        lng:element.geometry.location.lng
+        console.log(element.geometry.location);
+        let ubication = {
+            lat: element.geometry.location.lat,
+            lng: element.geometry.location.lng
         }
         drawMap(ubication)
     });
 }
-const handleError=() =>{
+const handleError = () => {
     console.log('Se ha presentado un error');
-  }
+}
 
-const drawMap=(objUbication)=>{
-    let containerMap=document.getElementById("map");
+const drawMap = (objUbication) => {
+    let containerMap = document.getElementById("map");
     console.log(objUbication);
- let map=new google.maps.Map(containerMap,{
-        center:objUbication,
-        zoom:4
- })
- let cpMaker=new google.maps.Marker({
-     position:objUbication,
-     title:"CP"
- })
+    let map = new google.maps.Map(containerMap, {
+        center: objUbication,
+        zoom: 4
+    })
+    let cpMaker = new google.maps.Marker({
+        position: objUbication,
+        title: "CP"
+    })
     return cpMaker.setMap(map);
 }
 
